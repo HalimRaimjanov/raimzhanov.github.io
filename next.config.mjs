d97aa7afce190fb8e309553850b41f93e4dcf3d6
Halim <halimjonraimjanov@gmail.com>
2025-02-16 02:34:43 +0600

    fixed bags

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,20 @@ const withMDX = mdx({
   extension: /\.mdx?$/,
   options: {},
 });
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  pageExtensions: ["ts", "tsx", "md", "mdx"],
+};
+
+export default withMDX(nextConfig);
+
+/*import mdx from "@next/mdx";
+
+const withMDX = mdx({
+  extension: /\.mdx?$/,
+  options: {},
+});
 // next.config.mjs
 export default {
   output: "export",
@@ -13,5 +27,5 @@ export default {
   basePath: "/raimzhanov.github.io", // Укажи правильное имя репозитория
   assetPrefix: "/raimzhanov.github.io/", // Правильные пути для статики
 };
-
+*/
 
